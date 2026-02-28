8395096957ff6cd46a9d3843a67312e5b3f4ace8
iviartin <mramirez@laurentian.ca>
2026-02-28 11:48:09 -0500

    GetComputerChoice Function Added & Tested

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello Worlds")
+// console.log("Hello World")
+
+/// Adding getComputerChoice: Returns RPS)
+function getComputerChoice() {
+    const rand = Math.random();
+
+    if (rand <= 0.33) {
+        return "rock";
+    } else if (rand <= 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+console.log(getComputerChoice())
